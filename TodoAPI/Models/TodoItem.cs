1ffb850ce031ea41af8dbf9176478594cd4555e6
jura <jirapanek@gmail.com>
2023-01-16 10:22:46 +0100

    Add TodoItem model.

---
+++ TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Models;
+
+public class TodoItem
+{
+	public string Name { get; set; } = default!;
+	public int Priority { get; set; }
+	public Status Status { get; set; }
+}
+
+public enum Status
+{
+	[Display(Name = "Not started")]
+	NotStarted,
+	[Display(Name = "In progress")]
+	InProgress,
+	[Display(Name = "Completed")]
+	Completed
+}
